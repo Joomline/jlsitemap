e573f58f2a9f8c05f528c8982a255fbe7d1a8c1a
Vadim Kunitsyn <zikkuratvk@gmail.com>
2018-11-02 12:51:48 +0200

    info block

--- com_jlsitemap/views/home/tmpl/default.php
+++ com_jlsitemap/views/home/tmpl/default.php
@@ -20,60 +20,71 @@ HTMLHelper::_('stylesheet', 'media/com_jlsitemap/css/admin.min.css', array('vers
 ?>
 
 <div class="row-fluid">
-	<div class="row-fluid icons-block">
-		<div class="span2">
-			<a class="item"
-			   href="<?php echo Route::_('index.php?option=com_jlsitemap&task=generate'); ?>">
-				<div class="img">
-					<span class="icon-play large-icon"></span>
-				</div>
-				<div class="title">
-					<?php echo Text::_('COM_JLSITEMAP_GENERATION'); ?>
-				</div>
-			</a>
-		</div>
-		<div class="span2">
-			<?php if (File::exists(JPATH_ROOT . '/sitemap.xml')): ?>
-				<a class="item" href="<?php echo Uri::root() . 'sitemap.xml'; ?>" target="_blank">
+	<div class="span8">
+		<div class="row-fluid icons-block">
+			<div class="span3">
+				<a class="item"
+				   href="<?php echo Route::_('index.php?option=com_jlsitemap&task=generate'); ?>">
 					<div class="img">
-						<span class="icon-tree-2 large-icon"></span>
+						<span class="icon-play large-icon"></span>
 					</div>
 					<div class="title">
-						<?php echo Text::_('COM_JLSITEMAP_SITEMAP'); ?>
+						<?php echo Text::_('COM_JLSITEMAP_GENERATION'); ?>
 					</div>
 				</a>
-			<?php else: ?>
-				<div class="item not-active">
+			</div>
+			<div class="span3">
+				<?php if (File::exists(JPATH_ROOT . '/sitemap.xml')): ?>
+					<a class="item" href="<?php echo Uri::root() . 'sitemap.xml'; ?>" target="_blank">
+						<div class="img">
+							<span class="icon-tree-2 large-icon"></span>
+						</div>
+						<div class="title">
+							<?php echo Text::_('COM_JLSITEMAP_SITEMAP'); ?>
+						</div>
+					</a>
+				<?php else: ?>
+					<div class="item not-active">
+						<div class="img">
+							<span class="icon-tree-2 large-icon"></span>
+						</div>
+						<div class="title text-error">
+							<?php echo Text::_('COM_JLSITEMAP_ERROR_SITEMAP_NOT_FOUND'); ?>
+						</div>
+					</div>
+				<?php endif; ?>
+			</div>
+			<div class="span3">
+				<a class="item" href="<?php echo Route::_('index.php?option=com_plugins&filter[folder]=jlsitemap'); ?>"
+				   target="_blank">
 					<div class="img">
-						<span class="icon-tree-2 large-icon"></span>
+						<span class="icon-power-cord large-icon"></span>
 					</div>
-					<div class="title text-error">
-						<?php echo Text::_('COM_JLSITEMAP_ERROR_SITEMAP_NOT_FOUND'); ?>
+					<div class="title">
+						<?php echo Text::_('COM_JLSITEMAP_PLUGINS'); ?>
 					</div>
-				</div>
-			<?php endif; ?>
+				</a>
+			</div>
+			<div class="span3">
+				<a class="item"
+				   href="<?php echo Route::_('index.php?option=com_config&view=component&component=com_jlsitemap'); ?>">
+					<div class="img">
+						<span class="icon-options large-icon"></span>
+					</div>
+					<div class="title">
+						<?php echo Text::_('COM_JLSITEMAP_CONFIG'); ?>
+					</div>
+				</a>
+			</div>
 		</div>
-		<div class="span2">
-			<a class="item" href="<?php echo Route::_('index.php?option=com_plugins&filter[folder]=jlsitemap'); ?>"
-			   target="_blank">
-				<div class="img">
-					<span class="icon-power-cord large-icon"></span>
-				</div>
+	</div>
+	<div class="span4">
+		<div class="row-fluid icons-block">	
+			<div class="item">
 				<div class="title">
-					<?php echo Text::_('COM_JLSITEMAP_PLUGINS'); ?>
+					<?php echo Text::_('COM_JLSITEMAP_ADMIN_TEXT'); ?>					
 				</div>
-			</a>
-		</div>
-		<div class="span2">
-			<a class="item"
-			   href="<?php echo Route::_('index.php?option=com_config&view=component&component=com_jlsitemap'); ?>">
-				<div class="img">
-					<span class="icon-options large-icon"></span>
-				</div>
-				<div class="title">
-					<?php echo Text::_('COM_JLSITEMAP_CONFIG'); ?>
-				</div>
-			</a>
+			</div>
 		</div>
 	</div>
 </div>
