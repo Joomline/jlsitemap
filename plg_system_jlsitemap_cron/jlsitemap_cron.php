b53ac238906652f73062a299ffb085e2f443be7e
Septdir <septdir@gmail.com>
2018-11-02 16:03:49 +0300

    Add generation function

--- plg_system_jlsitemap_cron/jlsitemap_cron.php
+++ plg_system_jlsitemap_cron/jlsitemap_cron.php
@@ -10,3 +10,55 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\Model\BaseDatabaseModel;
+use Joomla\CMS\Plugin\CMSPlugin;
+
+class PlgSystemJLSitemap_Cron extends CMSPlugin
+{
+	/**
+	 * Affects constructor behavior. If true, language files will be loaded automatically.
+	 *
+	 * @var boolean
+	 *
+	 * @since 0.0.2
+	 */
+	protected $autoloadLanguage = true;
+
+	/**
+	 * Run cron
+	 *
+	 * @since 0.0.2
+	 */
+	public function onAjaxJLSitemap_Cron()
+	{
+
+	}
+
+	/**
+	 * Method to generate site map
+	 *
+	 * @return boolean|object
+	 *
+	 * @since 0.0.2
+	 */
+	protected function generate()
+	{
+		try
+		{
+			BaseDatabaseModel::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_jlsitemap/models');
+			$model = BaseDatabaseModel::getInstance('Generation', 'JLSitemapModel', array('ignore_request' => true));
+
+			if (!$urls = $model->generate())
+			{
+				throw new Exception(Text::sprintf('PLG_SYSTEM_JLSITEMAP_GENERATION_FAILURE', $model->getError()));
+			}
+
+			return $urls;
+		}
+		catch (Exception $e)
+		{
+			throw new Exception(Text::sprintf('PLG_SYSTEM_JLSITEMAP_GENERATION_FAILURE', $e->getMessage()));
+		}
+	}
+}
