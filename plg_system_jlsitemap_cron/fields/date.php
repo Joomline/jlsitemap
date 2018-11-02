368ddf5cefbbd08703b4822ef5e399e37c887a32
Septdir <septdir@gmail.com>
2018-11-03 00:45:16 +0300

    Add date field

---
+++ plg_system_jlsitemap_cron/fields/date.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package    System - JLSitemap Cron Plugin
+ * @version    1.0.0
+ * @author     Joomline - joomline.ru
+ * @copyright  Copyright (c) 2010 - 2018 Joomline. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://joomline.ru/
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Form\FormField;
+
+class JFormFieldDate extends FormField
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var string
+	 *
+	 * @since 1.0.1
+	 */
+	protected $type = 'key';
+
+	/**
+	 * Name of the layout being used to render the field
+	 *
+	 * @var string
+	 *
+	 * @since 1.0.1
+	 */
+	protected $layout = 'plugins.system.jlsitemap_cron.fields.date';
+}
---
+++ plg_system_jlsitemap_cron/layouts/fields/date.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package    System - JLSitemap Cron Plugin
+ * @version    1.0.0
+ * @author     Joomline - joomline.ru
+ * @copyright  Copyright (c) 2010 - 2018 Joomline. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://joomline.ru/
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Factory;
+
+extract($displayData);
+
+/**
+ * Layout variables
+ * -----------------
+ * @var  string $id    DOM id of the field.
+ * @var  string $name  Name of the input field.
+ * @var  string $value Value attribute of the field.
+ */
+
+$date = (empty($value)) ? Text::_('JNEVER') : HTMLHelper::_('date', $value, Text::_('DATE_FORMAT_LC2')) .
+	' (' . Factory::getConfig()->get('offset') . ')'
+?>
+<div data-input-date="<?php echo $id; ?>">
+	<input type="text" readonly value="<?php echo $date; ?>">
+	<input id="<?php echo $id; ?>" type="hidden" readonly value="<?php echo $value; ?>">
+</div>
