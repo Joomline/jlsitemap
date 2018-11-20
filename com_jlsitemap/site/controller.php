e434ef7a2a7b54ad093d596099dffcdf0d6bde2c
Septdir <septdir@gmail.com>
2018-11-20 19:47:27 +0300

    Unset message if don't redirect response

--- com_jlsitemap/site/controller.php
+++ com_jlsitemap/site/controller.php
@@ -71,20 +71,26 @@ class JLSiteMapController extends BaseController
 	 */
 	protected function setResponse($type = null, $data = null)
 	{
-		$app = Factory::getApplication();
+		$app     = Factory::getApplication();
+		$message = $this->message;
 
 		if (empty($type))
 		{
 			$type = $this->input->get('response', 'redirect');
 		}
 
+		// Unset message if don't redirect response
+		if ($type != 'redirect')
+		{
+			$this->setMessage(null);
+		}
+
 		// Admin response
 		if ($type == 'admin')
 		{
 			$name    = 'jlsitemap_generation';
-			$value   = new JsonResponse('', $this->message, !empty($this->_errors));
+			$value   = new JsonResponse('', $message, !empty($this->_errors));
 			$expires = Factory::getDate('+1 day')->toUnix();
-
 
 			$app->input->cookie->set($name, $value, $expires, $app->get('cookie_path', '/'),
 				$app->get('cookie_domain'), $app->isSSLConnection());
@@ -99,7 +105,7 @@ class JLSiteMapController extends BaseController
 		if ($type == 'json')
 		{
 			$response = (!empty($data['response'])) ? $data['response'] : '';
-			echo new JsonResponse($response, $this->message, !empty($this->_errors));
+			echo new JsonResponse($response, $message, !empty($this->_errors));
 
 			$app->close();
 
@@ -111,7 +117,7 @@ class JLSiteMapController extends BaseController
 		{
 			if (!empty($this->_errors))
 			{
-				throw new Exception($this->message);
+				throw new Exception($message);
 			}
 
 			echo '<pre>', print_r($data, true), '</pre>';
@@ -129,5 +135,31 @@ class JLSiteMapController extends BaseController
 		$this->setRedirect(Route::_($redirect, false));
 
 		return true;
+	}
+
+	/**
+	 * Redirects the browser or returns false if no redirect is set.
+	 *
+	 * @return  boolean  False if no redirect exists.
+	 *
+	 * @since   1.2.1
+	 */
+	public function redirect()
+	{
+		if ($this->redirect)
+		{
+			$app = Factory::getApplication();
+
+			// Enqueue the redirect message
+			if (!empty($this->message))
+			{
+				$app->enqueueMessage($this->message, $this->messageType);
+			}
+
+			// Execute the redirect
+			$app->redirect($this->redirect);
+		}
+
+		return false;
 	}
 }
