9d9a4fe8af934c1d2354d32636a15afebfdcb6fa
Septdir <septdir@gmail.com>
2018-11-28 17:08:02 +0300

    Improvement plugin install script

--- plg_system_jlsitemap_cron/script.php
+++ plg_system_jlsitemap_cron/script.php
@@ -12,19 +12,17 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Filesystem\Folder;
+use Joomla\CMS\Installer\InstallerAdapter;
 
 class PlgSystemJLSitemap_CronInstallerScript
 {
 	/**
-	 * Enable plugin after installation
+	 * Runs right after any installation action.
 	 *
-	 * @param  string $type      Type of PostFlight action. Possible values are:
-	 *                           - * install
-	 *                           - * update
-	 *                           - * discover_install
-	 * @param         $parent    Parent object calling object.
+	 * @param  string           $type   Type of PostFlight action. Possible values are:
+	 * @param  InstallerAdapter $parent Parent object calling object.
 	 *
-	 * @return bool
+	 * @return void
 	 *
 	 * @since 0.0.1
 	 */
@@ -33,49 +31,132 @@ class PlgSystemJLSitemap_CronInstallerScript
 		// Enable plugin
 		if ($type == 'install')
 		{
-			// Prepare plugin object
-			$plugin          = new stdClass();
-			$plugin->type    = 'plugin';
-			$plugin->element = 'jlsitemap_cron';
-			$plugin->folder  = 'system';
-			$plugin->enabled = 1;
-
-			// Update record
-			Factory::getDbo()->updateObject('#__extensions', $plugin, array('type', 'element', 'folder'));
+			$this->enablePlugin($parent);
 		}
 
-		// Move layouts
-		$src  = JPATH_PLUGINS . '/system/jlsitemap_cron/layouts';
-		$dest = JPATH_ROOT . '/layouts/plugins/system';
+		// Install layouts
+		$this->installLayouts($parent);
+	}
 
-		// System plugins layouts path check
-		if (!Folder::exists($dest))
+	/**
+	 * Enable plugin after installation
+	 *
+	 * @param  InstallerAdapter $parent Parent object calling object.
+	 *
+	 * @return void
+	 *
+	 * @since 1.3.1
+	 */
+	protected function enablePlugin($parent)
+	{
+		// Prepare plugin object
+		$plugin          = new stdClass();
+		$plugin->type    = 'plugin';
+		$plugin->element = $parent->getElement();
+		$plugin->folder  = (string) $parent->getParent()->manifest->attributes()['group'];
+		$plugin->enabled = 1;
+
+		// Update record
+		Factory::getDbo()->updateObject('#__extensions', $plugin, array('type', 'element', 'folder'));
+	}
+
+	/**
+	 * Method to install/update extension layouts
+	 *
+	 * @param  InstallerAdapter $parent Parent object calling object.
+	 *
+	 * @return void
+	 *
+	 * @since  1.3.1
+	 */
+	protected function installLayouts($parent)
+	{
+		$root   = JPATH_ROOT . '/layouts';
+		$source = $parent->getParent()->getPath('source');
+
+		// Get attributes
+		$attributes = $parent->getParent()->manifest->xpath('layouts');
+		if (!is_array($attributes) || empty($attributes[0])) return;
+
+		// Get destination
+		$destination = (!empty($attributes[0]->attributes()->destination)) ?
+			(string) $attributes[0]->attributes()->destination : false;
+		if (!$destination) return;
+
+		// Remove old layouts
+		if (Folder::exists($root . '/' . trim($destination, '/')))
 		{
-			Folder::create($dest);
+			Folder::delete($root . '/' . trim($destination, '/'));
 		}
-		$dest .= '/jlsitemap_cron';
 
-		// Delete old layouts
-		if (Folder::exists($dest))
+		// Get folder
+		$folder = (!empty($attributes[0]->attributes()->folder)) ?
+			(string) $attributes[0]->attributes()->folder : 'layouts';
+		if (!Folder::exists($source . '/' . trim($folder, '/'))) return;
+
+		// Prepare src and dest
+		$src  = $source . '/' . trim($folder, '/');
+		$dest = $root . '/' . trim($destination, '/');
+
+		// Check destination
+		$path = $root;
+		$dirs = explode('/', $destination);
+		array_pop($dirs);
+
+		if (!empty($dirs))
 		{
-			Folder::delete($dest);
+			foreach ($dirs as $i => $dir)
+			{
+				$path .= '/' . $dir;
+				if (!Folder::exists($path))
+				{
+					Folder::create($path);
+				}
+			}
 		}
 
 		// Move layouts
 		Folder::move($src, $dest);
-
-		return true;
 	}
 
 	/**
-	 * Remove layouts
+	 * This method is called after extension is uninstalled.
 	 *
-	 * @param   JAdapterInstance $adapter The object responsible for running this script
+	 * @param  InstallerAdapter $parent Parent object calling object.
 	 *
-	 * @since 0.0.2
+	 * @return void
+	 *
+	 * @since  0.0.2
 	 */
-	public function uninstall(JAdapterInstance $adapter)
+	public function uninstall($parent)
 	{
-		Folder::delete(JPATH_ROOT . '/layouts/plugins/system/jlsitemap_cron');
+		// Uninstall layouts
+		$this->uninstallLayouts($parent);
+	}
+
+	/**
+	 * Method to uninstall extension layouts
+	 *
+	 * @param  InstallerAdapter $parent Parent object calling object.
+	 *
+	 * @return void
+	 *
+	 * @since  1.3.1
+	 */
+	protected function uninstallLayouts($parent)
+	{
+		$attributes = $parent->getParent()->manifest->xpath('layouts');
+		if (!is_array($attributes) || empty($attributes[0])) return;
+
+		$destination = (!empty($attributes[0]->attributes()->destination)) ?
+			(string) $attributes[0]->attributes()->destination : false;
+		if (!$destination) return;
+
+		$dest = JPATH_ROOT . '/layouts/' . trim($destination, '/');
+
+		if (Folder::exists($dest))
+		{
+			Folder::delete($dest);
+		}
 	}
 }
