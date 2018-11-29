d4377baa4f33706facfa693cf02a5257d0d99437
Septdir <septdir@gmail.com>
2018-11-29 18:12:24 +0300

    Change addAccessKey function

--- com_jlsitemap/script.php
+++ com_jlsitemap/script.php
@@ -10,8 +10,6 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Filesystem\File;
 use Joomla\CMS\Filesystem\Folder;
 use Joomla\CMS\Installer\InstallerAdapter;
@@ -105,26 +103,9 @@ class com_jlsitemapInstallerScript
 	 */
 	protected function addAccessKey()
 	{
-		$params = ComponentHelper::getComponent('com_jlsitemap')->getParams();
-		if (empty($params->get('access_key')))
-		{
-			// Prepare access key
-			$access_key = '';
-			$values     = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's',
-				't', 'u', 'v', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-				'P', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-			for ($i = 0; $i < 15; $i++)
-			{
-				$key        = rand(0, count($values) - 1);
-				$access_key .= $values[$key];
-			}
-			$params->set('access_key', $access_key);
+		JLoader::register('JLSitemapHelperSecrets', JPATH_ADMINISTRATOR . '/components/com_jlsitemap/helpers/secrets.php');
 
-			$component          = new stdClass();
-			$component->element = 'com_jlsitemap';
-			$component->params  = (string) $params;
-			Factory::getDbo()->updateObject('#__extensions', $component, array('element'));
-		}
+		JLSitemapHelperSecrets::getAccessKey();
 	}
 
 	/**
