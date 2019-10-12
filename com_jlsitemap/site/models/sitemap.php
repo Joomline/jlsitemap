d261a0e2f75cc62d181a0f93eb5d7337f4630f18
Igor Berdichevkskiy <septdir@gmail.com>
2019-10-12 16:11:45 +0300

    Simplified root

--- com_jlsitemap/site/models/sitemap.php
+++ com_jlsitemap/site/models/sitemap.php
@@ -149,7 +149,6 @@ class JLSitemapModelSitemap extends BaseDatabaseModel
 		}
 
 		// Main sitemap
-		$root    = rtrim(Uri::root(), '/');
 		$date    = Factory::getDate();
 		$sitemap = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?>'
 			. '<!-- JLSitemap ' . $date->toSql() . ' -->'
@@ -157,7 +156,7 @@ class JLSitemapModelSitemap extends BaseDatabaseModel
 		for ($i = 1; $i <= $f; $i++)
 		{
 			$child = $sitemap->addChild('sitemap');
-			$child->addChild('loc', $root . '/sitemap_' . $i . '.xml');
+			$child->addChild('loc', Uri::root() . 'sitemap_' . $i . '.xml');
 			$child->addChild('lastmod', $date->toISO8601());
 		}
 		$xml = $sitemap->asXML();
