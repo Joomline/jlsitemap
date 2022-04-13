5bff66828c32cced4d46f3d81e2d76ba6dc39c99
Vadim Kunitsyn <zikkuratvk@gmail.com>
2022-04-13 17:09:26 +0200

    Copyright update

--- plg_jlsitemap_kunena/kunena.php
+++ plg_jlsitemap_kunena/kunena.php
@@ -3,7 +3,7 @@
  * @package    JLSitemap - Kunena Plugin
  * @version    @version@
  * @author     Joomline - joomline.ru
- * @copyright  Copyright (c) 2010 - 2020 Joomline. All rights reserved.
+ * @copyright  Copyright (c) 2010 - 2022 Joomline. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
  * @link       https://joomline.ru/
  */
